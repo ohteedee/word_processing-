9cc8e90f54650a6ddb5b1ca30b2286eb6d8dd55f
ohteedee <toshine4u2007@yahoo.com>
2021-12-07 12:24:28 +0100

    hello added

--- src/word_processor.py
+++ src/word_processor.py
@@ -9,10 +9,11 @@ to get the coverage of tests.
 Open the index.html in the browser.
 """
 
+# hello there
 
 def count_words(text):
     """
-    This function counts some words and return integer which is the length of words
+    This function counts some words and return integer which is the length of wordsg
     """
     if not isinstance(text, str):
         raise TypeError("only accepts strings")
